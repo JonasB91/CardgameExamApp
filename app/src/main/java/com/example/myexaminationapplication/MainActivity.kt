29db5bf1b57bde522f1e706560e9cc81a402b65d
oscarsparrman <oscar.sparrman@student.kyh.se>
2022-09-22 12:04:07 +0200

    tog bort funktion som inte behövde och la till logig för för hålla koll på scorei.

--- app/src/main/java/com/example/myexaminationapplication/MainActivity.kt
+++ app/src/main/java/com/example/myexaminationapplication/MainActivity.kt
@@ -17,25 +17,36 @@ class Card( var value: Int,
 class MainActivity : AppCompatActivity() {
     lateinit var textView: TextView
     lateinit var imageView: ImageView
+    lateinit var scoreView: TextView
     var cardList = mutableListOf<Card>()
+    var score: Int = 0
+    var firstCard: Card? = null
+    var secondCard: Card? =null
+    var scoreString: String =""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        scoreView =findViewById(R.id.textViewScore)
         textView = findViewById(R.id.questionView)
         imageView = findViewById(R.id.mainImage)
 
         val buttonHigh = findViewById<Button>(R.id.buttonPressHigh)
         buttonHigh.setOnClickListener {
-            buttonPressed()
+            setNewrandomCard()
+            checkAnswerRight()
         }
         val buttonLow = findViewById<Button>(R.id.buttonPressLow)
         buttonLow.setOnClickListener {
-            buttonPressed()
+            setNewrandomCard()
+            checkAnswerleft()
+
         }
 
             cardsLibrary()
+
+
 
     }
 
@@ -160,77 +171,63 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun getImageResource(number: String): Int {
-        return when (number) {
-            "1" -> R.drawable.clubsoface
-            "2" -> R.drawable.clubsoftwo
-            "3" -> R.drawable.clubsofthree
-            "4" -> R.drawable.clubsoffour
-            "5" -> R.drawable.clubsoffive
-            "6" -> R.drawable.clubsofsix
-            "7" -> R.drawable.clubsofseven
-            "8" -> R.drawable.clubsofeight
-            "9" -> R.drawable.clubsofnine
-            "10" -> R.drawable.clubsoften
-            "11" -> R.drawable.clubsofjack
-            "12" -> R.drawable.clubsofqueen
-            "13" -> R.drawable.clubsofking
 
-            "14" -> R.drawable.diamondoface
-            "15" -> R.drawable.diamondoftwo
-            "16" -> R.drawable.diamondofthree
-            "17" -> R.drawable.diamondoffour
-            "18" -> R.drawable.diamondoffive
-            "19" -> R.drawable.diamondofsix
-            "20" -> R.drawable.diamondofseven
-            "21" -> R.drawable.diamondofeight
-            "22" -> R.drawable.diamondofnine
-            "23" -> R.drawable.diamondoften
-            "24" -> R.drawable.diamondofjack
-            "25" -> R.drawable.diamondofqueen
-            "26" -> R.drawable.diamondofking
+    fun checkAnswerRight(): Boolean {
 
-            "27" -> R.drawable.heartoface
-            "28" -> R.drawable.heartoftwo
-            "29" -> R.drawable.heartofthree
-            "30" -> R.drawable.heartoffour
-            "31" -> R.drawable.heartoffive
-            "32" -> R.drawable.heartofsix
-            "33" -> R.drawable.heartofseven
-            "34" -> R.drawable.heartofeight
-            "35" -> R.drawable.heartofnine
-            "36" -> R.drawable.heartoften
-            "37" -> R.drawable.heartofjack
-            "38" -> R.drawable.heartofqueen
-            "39" -> R.drawable.heartofking
+        setNewrandomCard()
 
-            "40" -> R.drawable.spadeoface
-            "41" -> R.drawable.spadeoftwo
-            "42" -> R.drawable.spadeofthree
-            "43" -> R.drawable.spadeoffour
-            "44" -> R.drawable.spadeoffive
-            "45" -> R.drawable.spadeofsix
-            "46" -> R.drawable.spadeofseven
-            "47" -> R.drawable.spadeofeight
-            "48" -> R.drawable.spadeofnine
-            "49" -> R.drawable.spadeoften
-            "50" -> R.drawable.spadeofjack
-            "51" -> R.drawable.spadeofqueen
-            "52" -> R.drawable.spadeofking
-            else -> R.drawable.cardfrontside
+
+        if (firstCard.value > secondCard.value) {
+            score++
+
+             scoreString = score.toString()
+            scoreView.text = scoreString }
+//         else if (firstCardValue < secondCardValue){
+//            score --
+//        }
+        return firstCard.value > secondCard.value
+    }
+
+
+    fun checkAnswerleft(): Boolean {
+
+
+
+
+        if (firstCard.value < secondCard.value) {
+            score++
+
+             scoreString = score.toString()
+            scoreView.text = scoreString
         }
+//        else if (firstCardValue > secondCardValue){
+//            score --
+//        }
+
+        return firstCard.value < secondCard.value
     }
 
 
 
-    fun buttonPressed() {
 
 
+    fun setNewrandomCard(): Int {
+
+        secondCard = firstCard
         val randomIndex = Random.nextInt(cardList.size);
 
-        val randomElement = cardList[randomIndex]
+         firstCard = cardList[randomIndex]
 
-        imageView.setImageResource(randomElement.image)
+        //var firstCardValue = randomElement.value
+
+
+
+
+        imageView.setImageResource(firstCard.image)
+
+        return firstCard.value
+
+
     }
 
 
